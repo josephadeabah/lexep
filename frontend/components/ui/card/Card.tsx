b45b99957baf9c217fb6e2acd34dc3e86f748b0a
josephadeabah <abeansah.lx@gmail.com>
2026-08-28 18:38:16 +0000

    update

--- frontend/components/ui/card/Card.tsx
+++ frontend/components/ui/card/Card.tsx
@@ -1,7 +1,14 @@
 import { HTMLAttributes } from "react";
 import { cn } from "@/lib/utils";
-import styles from "./card.module.css";
 
 export function Card({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn(styles.card, className)} {...props} />;
+  return (
+    <div
+      className={cn(
+        "rounded-lg border border-[#e0d8c9] bg-white p-6 shadow-[0_4px_20px_rgba(0,0,0,0.04)]",
+        className
+      )}
+      {...props}
+    />
+  );
 }
