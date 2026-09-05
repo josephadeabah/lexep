f033661330d496cd59bc7a266210a6a47fd375ae
josephadeabah <abeansah.lx@gmail.com>
2026-09-05 14:51:38 +0000

    update next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
+  output: process.env.VERCEL ? undefined : "standalone",
 };
 
 export default nextConfig;
